fa8e95ae0ee50a88c1ab2bbe36e1e2342c096cba
Rochelle Deulley <rochelle.deulley@gmail.com>
2021-07-23 16:52:50 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	public string CreateList();
+    }
+
+
+}
+
+public class ItemsForList {
+    public List <string> Items {get; set;}
+
+    public List <string> CreateList(int amount, string item)
+    {
+
+    }
+}
